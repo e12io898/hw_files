5407884c8d21ed19483e6328a25a42c967cec7ee
e12io898 <e12io898@ya.ru>
2023-03-09 21:44:40 +0300

    comm2

--- task_3/main.py
+++ task_3/main.py
@@ -5,10 +5,10 @@ for i in range(3):
     with open(file_name, encoding='utf-8') as f:
         buffer[file_name] = f.readlines()
 
-sort_buffer = dict(sorted(buffer.items(), key=lambda x: len(x[1])))
+sort_buffer = sorted((buffer.items()), key=lambda x: len(x[1]))
 
 with open('sorted.txt', 'a', encoding='utf-8') as text:
-    for key, i in sort_buffer.items():
+    for key, i in sort_buffer:
         text.write(f'{key}\n')
         text.write(f'{str(len(i))}\n')
         for _ in i:
