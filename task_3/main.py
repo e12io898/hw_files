78a21fe397192e3b8cc245ac54eb038fd0074d67
e12io898 <e12io898@ya.ru>
2023-03-09 21:10:22 +0300

    comm1

--- task_3/main.py
+++ task_3/main.py
@@ -1,14 +1,14 @@
-bufer = {}
+buffer = {}
 
 for i in range(3):
     file_name = f'{i + 1}.txt'
     with open(file_name, encoding='utf-8') as f:
-        bufer[file_name] = f.readlines()
+        buffer[file_name] = f.readlines()
 
-sort_bufer = dict(sorted(bufer.items(), key=lambda x: len(x[1])))
+sort_buffer = dict(sorted(buffer.items(), key=lambda x: len(x[1])))
 
 with open('sorted.txt', 'a', encoding='utf-8') as text:
-    for key, i in sort_bufer.items():
+    for key, i in sort_buffer.items():
         text.write(f'{key}\n')
         text.write(f'{str(len(i))}\n')
         for _ in i:
